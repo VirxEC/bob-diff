27c7ce6206c0c8533ac9090244d74caf50c89717
Virx <virx@virxcase.dev>
2025-06-27 10:24:02 -0400

    Update bob-lib

--- src/diff.rs
+++ src/diff.rs
@@ -1,27 +1,27 @@
 use anyhow::anyhow;
 use bob_lib::bobdiff;
-use std::{fs, path::PathBuf};
+use std::{fs, path::Path};
 
-pub fn command_diff(old: PathBuf, new: PathBuf) -> anyhow::Result<Vec<u8>> {
-    if !fs::exists(&old)? {
+pub fn command_diff(old: &Path, new: &Path) -> anyhow::Result<Vec<u8>> {
+    if !fs::exists(old)? {
         return Err(anyhow!("Directory doesn't exist: {old:?}"));
     }
 
-    if !fs::exists(&new)? {
+    if !fs::exists(new)? {
         return Err(anyhow!("Directory doesn't exist: {new:?}"));
     }
 
-    let diff = bobdiff::DirDiff::new(&old, &new);
+    let diff = bobdiff::DirDiff::new(old, new);
     Ok(diff.ser())
 }
 
-pub fn command_diff_apply(dir: PathBuf, buf: &[u8]) -> anyhow::Result<()> {
-    if !fs::exists(&dir)? {
+pub fn command_diff_apply(dir: &Path, buf: &[u8]) -> anyhow::Result<()> {
+    if !fs::exists(dir)? {
         return Err(anyhow!("Directory doesn't exist"));
     }
 
     let diff = bobdiff::DirDiff::deser(buf)?;
-    diff.apply_to(&dir, true)?;
+    diff.apply_to(dir, true)?;
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,55 @@
-use core::{ffi::CStr, slice};
-use std::path::PathBuf;
+#![warn(clippy::pedantic, clippy::nursery)]
+
+use core::{
+    ffi::{CStr, c_char},
+    slice,
+};
+use std::{mem, path::PathBuf};
 
 mod diff;
 
-fn cstr_to_path(cstr: *const std::os::raw::c_char) -> Option<PathBuf> {
+fn cstr_to_path(cstr: *const c_char) -> Option<PathBuf> {
     let cstr = unsafe { CStr::from_ptr(cstr) };
     Some(PathBuf::from(cstr.to_str().ok()?))
 }
 
-fn vec_to_buf(vec: Vec<u8>) -> (*mut std::os::raw::c_char, u32) {
-    let len = vec.len() as u32;
-    let buf = vec.as_ptr();
+fn vec_to_buf(mut buf: Vec<u8>) -> (*mut c_char, u64) {
+    buf.shrink_to_fit();
+    let mut buf = mem::ManuallyDrop::new(buf);
 
-    (buf as *mut std::os::raw::c_char, len)
+    (buf.as_mut_ptr().cast::<c_char>(), buf.len() as u64)
 }
 
-fn buf_to_slice(buf: *const std::os::raw::c_char, len: u32) -> &'static [u8] {
-    let buf = buf as *const u8;
-    let len = len as usize;
-
-    unsafe { slice::from_raw_parts(buf, len) }
+const fn buf_to_slice(buf: *const c_char, len: usize) -> &'static [u8] {
+    unsafe { slice::from_raw_parts(buf.cast::<u8>(), len) }
 }
 
 /// # Safety
 ///
-/// This function is unsafe because it dereferences raw pointers and assumes that the data they point to is valid.
+/// This function is unsafe because it dereferences raw pointers and assumes that they point to null-terminated strings.
 ///
-/// The caller must ensure that the pointers are valid and that the data they point to is valid for the duration of the call.
+/// The caller must ensure that the pointers `old` and `new` are valid and that the data they point to is valid for the duration of the call.
+/// If `old` or `new` is simply not valid UTF-8, the error code 1 will be returned.
+///
+/// `out_buf` and `out_buf_len` are never read, only written to - they do not have to point to valid data.
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn diff(
-    old: *const std::os::raw::c_char,
-    new: *const std::os::raw::c_char,
-    out_buf: *mut *mut std::os::raw::c_char,
-    out_buf_len: *mut u32,
+    old: *const c_char,
+    new: *const c_char,
+    out_buf: *mut *mut c_char,
+    out_buf_len: *mut u64,
 ) -> u16 {
     let Some(old) = cstr_to_path(old) else {
+        eprintln!("'old' did not contain valid utf-8");
         return 1;
     };
 
     let Some(new) = cstr_to_path(new) else {
+        eprintln!("'new' did not contain valid utf-8");
         return 1;
     };
 
-    match diff::command_diff(old, new) {
+    match diff::command_diff(&old, &new) {
         Ok(buf) => {
             let (buf, len) = vec_to_buf(buf);
 
@@ -62,22 +69,24 @@ pub unsafe extern "C" fn diff(
 
 /// # Safety
 ///
-/// This function is unsafe because it dereferences raw pointers and assumes that the data they point to is valid.
+/// This function is unsafe because it dereferences a raw pointer and assumes that it points to a null-terminated string.
 ///
-/// The caller must ensure that the pointers are valid and that the data they point to is valid for the duration of the call.
+/// The caller must ensure that the pointer `dir` is valid and that the data it points to is valid for the duration of the call.
+/// If `dir` is simply not valid UTF-8, the error code 1 will be returned.
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn diff_apply(
-    dir: *const std::os::raw::c_char,
-    buf: *const std::os::raw::c_char,
-    buf_len: u32,
-) -> u16 {
+pub unsafe extern "C" fn diff_apply(dir: *const c_char, buf: *const c_char, buf_len: u64) -> u16 {
     let Some(dir) = cstr_to_path(dir) else {
+        eprintln!("'dir' did not contain valid utf-8");
         return 1;
     };
 
-    let buf = buf_to_slice(buf, buf_len);
+    let Ok(len) = usize::try_from(buf_len) else {
+        return 1;
+    };
 
-    if let Err(e) = diff::command_diff_apply(dir, buf) {
+    let buf = buf_to_slice(buf, len);
+
+    if let Err(e) = diff::command_diff_apply(&dir, buf) {
         eprintln!("Error: {e:?}");
         1
     } else {
